aa2c4c2fbcda2307913a5717e7f0a8d03f0bc6c0
Eric Gonzalez <ericrgon@us.ibm.com>
2013-06-04 22:04:09 -0500

    Server code and Activity updates

--- android/src/com/ericrgon/GarageActivity.java
+++ android/src/com/ericrgon/GarageActivity.java
@@ -4,6 +4,7 @@ package com.ericrgon;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -13,34 +14,69 @@ import java.util.List;
  */
 public class GarageActivity extends CloudBackendActivity {
 
-    private Button buzzButton;
+    private Button mBuzzButton;
+    private TextView mView;
+    private TextView mState;
 
     private List<CloudEntity> logList = new LinkedList<CloudEntity>();
+
+    private static final String DOCUMENT_NAME = "Buzz";
+
+    private static final String OPEN = "open";
+    private static final String CLOSED = "closed";
+
+    private enum States {OPEN,CLOSED};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buzzButton = (Button) findViewById(R.id.buzz);
+        mBuzzButton = (Button) findViewById(R.id.buzz);
+        mView = (TextView) findViewById(R.id.logs);
+        mState = (TextView) findViewById(R.id.status);
     }
 
+    @Override
+    protected void onPostCreate() {
+        super.onPostCreate();
+
+        CloudCallbackHandler<List<CloudEntity>> handler = new CloudCallbackHandler<List<CloudEntity>>() {
+            @Override
+            public void onComplete(List<CloudEntity> results) {
+                logList = results;
+                updateLogs();
+            }
+        };
+
+        getCloudBackend().listByKind(DOCUMENT_NAME,CloudEntity.PROP_CREATED_AT, CloudQuery.Order.DESC,15,
+                CloudQuery.Scope.FUTURE_AND_PAST,handler);
+    }
 
     public void onBuzzButtonPressed(View view) {
-        CloudEntity newBuzz = new CloudEntity("Buzz");
+        CloudEntity newBuzz = new CloudEntity(DOCUMENT_NAME);
 
         CloudCallbackHandler<CloudEntity> handler = new CloudCallbackHandler<CloudEntity>() {
             @Override
             public void onComplete(CloudEntity results) {
                 //Save result and display
                 logList.add(0,results);
-
-                buzzButton.setEnabled(true);
+                updateLogs();
+                mBuzzButton.setEnabled(true);
             }
         };
 
         getCloudBackend().insert(newBuzz,handler);
 
-        buzzButton.setEnabled(false);
+        mBuzzButton.setEnabled(false);
     }
+
+    private void updateLogs() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(CloudEntity entity : logList){
+            stringBuilder.append(entity.getCreatedAt() + "\n");
+        }
+        mView.setText(stringBuilder.toString());
+    }
+
 }
